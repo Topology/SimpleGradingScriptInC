b4d36f8c25f25f79f57e50c0afbcc0a07afaf32d
Edward Wertz <edward.o.wertz@gmail.com>
2016-09-15 16:43:51 -0500

    existing grading script

---
+++ gradingscript/lastf/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 15, 2016
+ *      Author: top
+ */
+
+
+#include<stdio.h>
+
+
+int main(int argc, char** argv){
+	int c;
+	while((c = getchar()) != EOF)
+		putchar(c);
+
+	return 0;
+
+}
